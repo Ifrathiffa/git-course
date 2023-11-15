cd5577a2d1055ac2bf36768a344bc4e2d7e484df
Ifrath <ifrathiffa80@gmail.com>
2023-11-15 12:33:05 +0530

    Version 1

---
+++ beg.js
@@ -0,0 +1,16 @@
+let user=prompt("Enter the user name:", "");
+if (user=="Admin"){
+    let pass=prompt("Enter the password:", "");
+    if (pass=="TheMaster"){
+        console.log("Welcome!");
+    }else if (pass==="" || pass===null){
+        console.log("Cancelled");
+    
+    }else{
+        console.log("Wrong Password");
+    }
+}else if (user==="" || user===null){
+    console.log("Cancelled");
+}else {
+    console.log("I don't know you");
+}
